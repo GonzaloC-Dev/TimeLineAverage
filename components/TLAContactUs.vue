68b9a164bb1301ad3dce01beb019738b6df2f5b1
Gonzalo Cantú Treviño <camaleon199@hotmail.com>
2023-04-12 01:13:49 -0600

    All components created

---
+++ components/TLAContactUs.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <div>logo</div>
+    <div>Contact us</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TLAContactUsPage",
+};
+</script>
---
+++ components/TLAFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <div>Youtube</div>
+    <div>Facebook</div>
+    <div>Instagram</div>
+    <div>Contact us</div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TLAFooter",
+};
+</script>
