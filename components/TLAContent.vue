5fb9fd533e6e4b4216414a918fe960afd2ec6a4b
Gonzalo Cantú Treviño <camaleon199@hotmail.com>
2023-06-19 13:07:06 -0600

    Form added, with email validation

--- components/TLAContent.vue
+++ components/TLAContent.vue
@@ -1,25 +1,27 @@
 <template>
+  <!-- First father element -->
   <div
     :style="{
-      // backgroundColor: 'yellow',
+      //backgroundColor: 'yellow',
       width: '100%',
-      height: '80%',
+      height: '200%',
       display: 'flex',
       justifyContent: 'center',
       flexDirection: 'column',
       alignItems: 'center',
     }"
   >
+    <!-- 2 texts father element -->
     <div
       :style="{
-        // backgroundColor: 'green',
+        //backgroundColor: 'green',
         width: '80%',
-        height: '100%',
+        height: '50%',
       }"
     >
       <div
         :style="{
-          // backgroundColor: 'orange',
+          //backgroundColor: 'orange',
           width: '100%',
           height: '50%',
           display: 'flex',
@@ -29,7 +31,7 @@
       >
         <div
           :style="{
-            // backgroundColor: 'purple',
+            //backgroundColor: 'purple',
             width: '60%',
             height: '100%',
             display: 'flex',
@@ -52,17 +54,19 @@
                 textAlign: 'left',
               }"
             >
-              The average wait time for lines at banks and cinemas can vary
-              greatly depending on the location, time of day, and other factors.
-              However, I can provide some general guidelines for estimated wait
-              times based on my understanding of common wait times.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
             </p>
           </div>
         </div>
 
         <div
           :style="{
-            // backgroundColor: 'brown',
+            //backgroundColor: 'brown',
             width: '40%',
             height: '100%',
             display: 'flex',
@@ -74,7 +78,7 @@
 
       <div
         :style="{
-          // backgroundColor: 'gray',
+          //backgroundColor: 'gray',
           width: '100%',
           height: '50%',
           display: 'flex',
@@ -84,7 +88,7 @@
       >
         <div
           :style="{
-            // backgroundColor: 'brown',
+            //backgroundColor: 'brown',
             width: '40%',
             height: '100%',
             display: 'flex',
@@ -94,7 +98,7 @@
         ></div>
         <div
           :style="{
-            // backgroundColor: 'red',
+            //backgroundColor: 'red',
             width: '60%',
             height: '100%',
             display: 'flex',
@@ -104,7 +108,7 @@
         >
           <div
             :style="{
-              // backgroundColor: 'orange',
+              //backgroundColor: 'orange',
               width: '70%',
               height: '40%',
               display: 'flex',
@@ -118,12 +122,131 @@
                 textAlign: 'right',
               }"
             >
-              The average wait time for lines at banks and cinemas can vary
-              greatly depending on the location, time of day, and other factors.
-              However, I can provide some general guidelines for estimated wait
-              times based on my understanding of common wait times.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
             </p>
           </div>
+        </div>
+      </div>
+    </div>
+    <!-- Form element -->
+    <div
+      :style="{
+        // backgroundColor: '#F0A332',
+        width: '80%',
+        height: '50%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }"
+    >
+      <div
+        :style="{
+          //  backgroundColor: 'red',
+          width: '90%',
+          height: '90%',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }"
+      >
+        <div
+          :style="{
+            width: '100%',
+            height: '15%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }"
+        >
+          <h1>Envianos tu información de contacto:</h1>
+        </div>
+        <div
+          :style="{
+            width: '100%',
+            height: '15%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }"
+        >
+          <input
+            v-model="FullName"
+            class="form-control form-control-lg"
+            type="text"
+            placeholder="Nombre Completo"
+            aria-label=".form-control-lg example"
+          />
+        </div>
+        <div
+          :style="{
+            width: '100%',
+            height: '15%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }"
+        >
+          <input
+            v-model="BuisnessName"
+            class="form-control form-control-lg"
+            type="text"
+            placeholder="Nombre de la empresa"
+            aria-label=".form-control-lg example"
+          />
+        </div>
+        <div
+          :style="{
+            width: '100%',
+            height: '15%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }"
+        >
+          <input
+            v-model="contactEmail"
+            class="form-control form-control-lg"
+            type="email"
+            required
+            placeholder="Correo de contacto"
+            aria-label=".form-control-lg example"
+          />
+        </div>
+        <div
+          :style="{
+            width: '100%',
+            height: '15%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }"
+        >
+          <input
+            v-model="contactTellphone"
+            class="form-control form-control-lg"
+            type="text"
+            placeholder="Télefono de contacto"
+            aria-label=".form-control-lg example"
+          />
+        </div>
+        <div
+          :style="{
+            width: '100%',
+            height: '15%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }"
+        >
+          <input
+            @click="submitForm"
+            class="btn btn-primary btn-lg"
+            type="submit"
+            value="Enviar"
+          />
         </div>
       </div>
     </div>
@@ -131,7 +254,54 @@
 </template>
 
 <script>
+
+//Try use FormElementSize from other file:
+//import design from "../assets/componentsSizes/TLAContentSizes"
 export default {
   name: "TLAContentPage",
+  data: () => ({
+    FullName: "",
+    BuisnessName: "",
+    contactEmail: "",
+    contactTellphone: "",
+  }),
+  methods: {
+    submitForm() {
+      if (
+        this.FullName === "" ||
+        this.BuisnessName === "" ||
+        this.contactEmail === "" ||
+        this.contactTellphone === ""
+      ) {
+        alert("Verifica los campos");
+      } else {
+        if (
+          this.validateEmail(this.contactEmail)
+        ) {
+          alert("Se envió con éxito")
+          this.emptyAllInput();
+        } else {
+          //Correct route
+          this.emptyInput(this.contactEmail);
+          alert("Please enter a valid email address"); 
+        }
+        
+      }
+    },
+    emptyInput(inputName) {
+      return (inputName = "");
+    },
+    emptyAllInput() {
+      this.FullName = "";
+      this.BuisnessName = "";
+      this.contactEmail = "";
+      this.contactTellphone = "";
+    },
+    validateEmail(email){
+      let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return pattern.test(email);
+    }
+  },
+  
 };
 </script>
